3c6c6502732369e2588c58e88b6c0f316e7633ef
JoseCortez1 <jose.vazcortez@gmail.com>
2021-09-09 16:50:56 -0500

    pruebas para   de sumar

--- __test__/suma.test.js
+++ __test__/suma.test.js
@@ -9,4 +9,10 @@ describe("Pruebas para suma", () => {
 	it("El resultado es   3 de 1 + 2", () => {
 		expect(suma(1, 2)).toBe(3)
 	})
+	test("1+(-2) is -1", () => {
+		expect(suma(1, -2)).toBe(-1)
+	})
+	test("-1+ (-2) is -3", () => {
+		expect(suma(-1, -2)).toBe(-3)
+	})
 })
